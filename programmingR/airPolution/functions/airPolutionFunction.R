ad851a064dda5a7202d7a2dc06e2d3a9aa37abde
Zhiyin Pan <zhiyin.pan@gmail.com>
2016-10-06 22:39:07 -0700

    initial checking for programmingR assignment

---
+++ programmingR/airPolution/functions/airPolutionFunction.R
@@ -0,0 +1,14 @@
+getData <- function(directory="/Users/zhiyin/workspace/coursera/programmingR/airPolution/data", 
+                    pollutant, id=1:332) {
+
+    matrices <- matrix(ncol=4, nrow=0)
+    
+    for (i in files) {
+        fullFileDir <- paste(baseDir, dataDir, str_pad(i, 3, pad = "0"), sep = "/")
+        fullFileDir <- paste(fullFileDir, ".csv", sep="")
+        
+        matrices <- rbind(matrices, read.csv(fullFileDir))
+    }
+    
+    matrices
+}
